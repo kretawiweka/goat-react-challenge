40be5ef82bb0213a090bc48e7ee6f44eefec58b2
Kretawiweka Nuraga Sani <ekanuraga@gmail.com>
2022-12-28 13:53:31 +0700

    chore: slicing todolist component

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import './index.css'
+import './container.css'
 
 export const Container = ({ children }) => {
   return (
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,11 +1,29 @@
 import { Container } from '../../components'
-import './index.css'
+import './todolist.css'
 
 export const TodoList = () => {
   return (
     <Container>
       <div className="todolist">
         <h1>TODO LIST</h1>
+        <form className="add-form">
+          <div className="field">
+            <input className="input-todo" placeholder="Task name" />
+          </div>
+          <button className="submit-btn" type="submit">
+            Add
+          </button>
+        </form>
+        <div className="todolist__items">
+          <div className="todolist__item">
+            <div className="item">Finish an interview</div>
+            <button className="action-item">Delete</button>
+          </div>
+          <div className="todolist__item">
+            <div className="item">Finish an interview</div>
+            <button className="action-item">Delete</button>
+          </div>
+        </div>
       </div>
     </Container>
   )
